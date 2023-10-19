0117b59f4a8e476281316844839bc830f9344ef5
vise1350 <125251867+vise1350@users.noreply.github.com>
2023-10-19 13:07:07 +0530

    Add files via upload
    T6 fully functional

---
+++ product.js
@@ -0,0 +1,42 @@
+const products=[];
+const fs=require("fs")
+const path=require("path")
+module.exports=class Product{
+    constructor(t){
+        this.title=t
+
+    }
+    save(){
+        const p=path.join(path.dirname(require.main.filename),
+        "data",
+        "products.json");
+        fs.readFile(p,(err,fileContent)=>{
+            let products=[]
+            if(!err){
+                products=JSON.parse(fileContent)
+
+            }
+            products.push(this)
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err)
+            })
+        })
+    }
+
+    static fetchAll(){
+        const p=path.join(path.dirname(require.main.filename),
+        "data",
+        "products.json");
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return [];
+            }
+            return JSON.parse(fileContent)
+        })
+        return products;
+
+    }
+
+
+
+}
--- products.js
+++ products.js
@@ -1,17 +1,20 @@
 const path=require("path")
 const rootDir=require("../util/path")
 
+const Product=require("../models/product")
 exports.getProducts=(req,res,next)=>{
     res.sendFile(path.join(rootDir,"views","add-product.html"))
 }
 
 exports.postProducts=(req,res,next)=>{
-
+    const product=new Product(req.body.title)
+    product.save()
     console.log(req.body);
     res.redirect("/")
 }
 
 exports.getDefault=(req,res,next)=>{
+    const products=Product.fetchAll()
   
     res.sendFile(path.join(__dirname,"../","views","shop.html"))
 
